ce42a23e7f971d16f7f89110799ff69454382ce5
bharatvaj.ph <bharatvaj.ph@zohocorp.com>
2022-04-25 23:50:31 +0530

    Optimize vim startup time from 2s to ~0.07 seconds
    - Lazy load vim plugins after starting vim, only very important plugins
    such as editorconfig-vim, vim-unimpaired, etc are run at startup.
    - Add '<leader>ec' for editing vim files quickly.
    - Remap '<leader>bb' to :Make (vim-dispatch)
    
    Add .config/alacritty/windows.yml
    - This import is always done because alacritty doesn't provide a way to
      check it.
    
    IMPORTANT
    ---------
    Use 'cmd' as the default shell for windows along with clink. Much
    better workflow when compared with powershell - so slow man.
    
    - Add .config/cmd/profile.cmd
    - Add .config/cmd/aliases.cmd
    - Add .config/cmd/setup.ps1
    - Add .config/cmd/packages.txt

---
+++ .config/cmd/setup.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+# TODO Handle Admin user check
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+# Install the absolute essentials
+scoop install $(gc ~/.config/cmd/packages.txt)
