49210ee83d38be9a0f5a3aff33420c1c58a8eda7
bharatvaj.ph <bharatvaj.ph@zohocorp.com>
2022-04-27 19:28:58 +0530

    Properly setup gvim in windows

--- .config/cmd/aliases.cmd
+++ .config/cmd/aliases.cmd
@@ -2,4 +2,5 @@ doskey v=vim $*
 doskey vi=vim $*
 doskey n=nvim $*
 doskey g=git $*
+doskey e=%EDITOR% $*
 doskey l=%USERPROFILE%\.config\cmd\lfcd.cmd
--- .config/cmd/profile.cmd
+++ .config/cmd/profile.cmd
@@ -9,4 +9,7 @@ set XDG_CONFIG_HOME=%USERPROFILE%\.config
 set XDG_DATA_HOME=%USERPROFILE%\.local\share
 set XDG_CACHE_HOME=%USERPROFILE%\.cache
 
+set MYVIMRC=%XDG_CONFIG_HOME%\vim\vimrc
+set VIMINIT=source $MYVIMRC
+
 %USERPROFILE%\.config\cmd\aliases.cmd
