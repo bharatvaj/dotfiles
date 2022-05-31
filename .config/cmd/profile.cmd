96f5c9f92dc28b8cb3c486b8f02df8d272674c1d
Bharatvaj <-g>
2022-05-31 18:43:19 +0530

    Setup clink for cmd
    Add .config/clink/clink_settings
    
    Use eol=crlf for *.cmd files, lf with cmd causes isses

--- .config/cmd/profile.cmd
+++ .config/cmd/profile.cmd
@@ -5,3 +5,14 @@ prompt $P ∆
 
 @call %XDG_CONFIG_HOME%\cmd\environment.bat
 @call %XDG_CONFIG_HOME%\cmd\aliases.bat
+
+set VISUAL=vim
+set EDITOR=%VISUAL%
+set XDG_CONFIG_HOME=%USERPROFILE%\.config
+set XDG_DATA_HOME=%USERPROFILE%\.local\share
+set XDG_CACHE_HOME=%USERPROFILE%\.cache
+
+set MYVIMRC=%XDG_CONFIG_HOME%\vim\vimrc
+set VIMINIT=source $MYVIMRC
+
+%XDG_CONFIG_HOME%\cmd\aliases.cmd
