82d49e7e5a80ab99e82b77a8b64759d3eed71fac
Bharatvaj <bharatvaj@yahoo.com>
2022-07-28 00:43:02 +0530

    nvim: Move from vimrc to init.lua
    Add: nvim: Hyper Red theme
    
    Modify: songdl: do `mpc update` after download
    
    Modify: lynx: video, audio, downlaod, external browser integration
    
    NOTE: I love MY hyper-red theme. It's my new favourite. My shell
    feels like home after a long time :)
    
    BUG: Sometimes vim is sourcing neovim runtime files.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,53 @@
 vim.g.mapleader = ' '
 local set = vim.opt
+local g = vim.g
 
 set.packpath="$XDG_DATA_HOME/nvim"
 
 local map = require("utils").map
+require("keybindings")
 
-map("n", "<Leader>ww", ":w<CR>", { silent = true })
-map("n", "<Leader>wq", ":wq<CR>", { silent = true })
-map("n", "<Leader>qq", ":q<CR>", { silent = true })
-map("n", "<CR>", ":noh<CR>", { silent = true })
 
 set.tabstop=4
-set.exrc=false
+set.number=true
+set.laststatus=1
+set.expandtab=false
 
+set.hlsearch=true
+set.incsearch=true
+set.wrapscan=false
+set.ignorecase=true
+set.smartcase=true
 
-require'lspconfig'.pyright.setup{}
+require'lspconfig'.clangd.setup{}
 
 set.shortmess="Iat"
+
+g.netrw_banner = 0
+g.netrw_liststyle = 3
+
+require('nvim_comment').setup()
+
+vim.cmd([[
+function! s:load_plugins(t) abort
+	packadd vim-fugitive
+endfunction
+
+augroup user_cmds
+	autocmd!
+	autocmd VimEnter * call timer_start(20, function('s:load_plugins'))
+augroup END
+]])
+
+-- theme
+-- hyper-red
+-- TODO use lua api
+vim.cmd('hi StatusLine ctermbg=white ctermfg=red')
+vim.cmd('hi StatusLineNC ctermbg=white ctermfg=black')
+vim.cmd('hi LineNr ctermbg=NONE ctermfg=red')
+vim.cmd('set fillchars=vert:\\│')
+vim.cmd('hi VertSplit term=NONE cterm=NONE gui=NONE')
+vim.cmd('hi SignColumn ctermbg=NONE guibg=NONE')
+
+
+
--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,10 +1,113 @@
+-- keybindings
+local set = vim.opt
+
+function save_and_build()
+-- TODO require vim-dispatch
+		vim.cmd('wall')
+		vim.cmd('Make')
+end
+
+function toggle_list(is_local_buffer)
+	-- TODO implement toggle for all buffers
+	local optset=vim.o
+	if optset.list then
+		optset.list=false
+	else
+		optset.list=true
+	end
+end
+
+function file_mv_helper()
+	vim.cmd('normal! 0i"A"0y$A p0imv j0')
+end
+
+-- Run Make
 local map = require("utils").map
+-- general
+map("n", "<leader>co", ":copen<cr>", { silent = false })
+map("n", "<leader>cs", ":cdo<space>s/", { silent = false })
+map("n", "<leader>ec", ":edit $XDG_CONFIG_HOME/nvim/init.lua<cr>", { silent = true })
+map("n", "<leader>ek", ":edit $XDG_CONFIG_HOME/nvim/lua/keybindings.lua<cr>", { silent = false })
+map("n", "<leader>et", ":set expandtab<cr>", { silent = false })
+map("n", "<leader>ff", ":find *", { silent = false })
+map("n", "<leader>lb", ":ls<CR>:b<space>", { silent = false })
+map("n", "<leader>lo", ":lopen<cr>", { silent = false })
+map("n", "<leader>ls", ":ldo<space>s/", { silent = false })
+map("n", "<leader>p", ":Rg<cr>", { silent = false })
+map("n", "<leader>sc", ":source $XDG_CONFIG_HOME/nvim/init.lua<cr>", { silent = false })
+map("n", "<leader>so", ":source %<cr>", { silent = false })
+map("n", "<leader>tl", ":lua toggle_list(true)<CR>", { silent = true })
+map("n", "<leader>tt", ":tabnew<cr>", { silent = false })
+map("n", "<leader>vg", ":vimgrep<space>", { silent = false })
+
+-- quick write and quit
 map("n", "<Leader>ww", ":w<CR>", { silent = true })
 map("n", "<Leader>wq", ":wq<CR>", { silent = true })
 map("n", "<Leader>qq", ":q<CR>", { silent = true })
-map("n", "<CR>", ":noh<CR>", { silent = true })
+map("n", "<leader>cs", ":noh<CR>", { silent = true })
 
-map("n", "<leader>f", ":find ", { silent = false })
-map("n", "<leader>co", ":copen<cr>", { silent = false })
-map("n", "<leader>g", ":grep ", { silent = false})
-map("n", "<leader>ec", ":edit ~/.config/nvim/init.lua<cr>", { silent = false})
+-- buffers
+map("n", "<leader>sc", ":source ~/.config/vim/vimrc<enter>", { silent = true })
+map("n", "<leader>ss", ":split<enter>", { silent = true })
+map("n", "<leader>vv", ":vsplit<enter>", { silent = true })
+
+-- vimspector
+map("n", "<leader>dd", ":call vimspector#Launch()<CR>", { silent = true })
+map("n", "<leader>dc", ":call GotoWindow(g:vimspector_session_windows.code)<CR>", { silent = true })
+map("n", "<leader>dt", ":call GotoWindow(g:vimspector_session_windows.tagpage)<CR>", { silent = true })
+map("n", "<leader>dv", ":call GotoWindow(g:vimspector_session_windows.variables)<CR>", { silent = true })
+map("n", "<leader>dw", ":call GotoWindow(g:vimspector_session_windows.watches)<CR>", { silent = true })
+map("n", "<leader>ds", ":call GotoWindow(g:vimspector_session_windows.stack_trace)<CR>", { silent = true })
+map("n", "<leader>do", ":call GotoWindow(g:vimspector_session_windows.output)<CR>", { silent = true })
+map("n", "<leader>de", ":call vimspector#Reset()<CR>", { silent = true })
+
+map("n", "<leader>dtcb", ":call vimspector#CleanLineBreakpoint()<CR>", { silent = true })
+
+map("n", "<leader>dl", "<Plug>VimspectorStepInto", { silent = true })
+map("n", "<leader>dj", "<Plug>VimspectorStepOver", { silent = true })
+map("n", "<leader>dk", "<Plug>VimspectorStepOut", { silent = true })
+map("n", "<leader>d_", "<Plug>VimspectorRestart", { silent = true })
+map("n", "<leader>d<space>", ":call vimspector#Continue()<CR>", { silent = true })
+
+map("n", "<leader>drc", "<Plug>VimspectorRunToCursor", { silent = true })
+map("n", "<leader>dbp" , "<Plug>VimspectorToggleBreakpoint", { silent = true })
+map("n", "<leader>dcbp", "<Plug>VimspectorToggleConditionalBreakpoint", { silent = true })
+
+map("n", "<leader>bb", ":lua save_and_build()<CR>", { silent = false })
+
+-- quick navigation
+-- telscope.nvim
+map("n", "<leader><leader>", ":Telescope find_files<cr>", { silent = true })
+
+-- file helpers
+map("n", "<leader>fmv", "lua file_mv_helper()",  { silent = true })
+
+-- doc functions
+function doc_quick_underline(n)
+	if (n == 1) then
+		vim.cmd('normal! yypv$r=')
+	else
+		vim.cmd('normal! yypv$r-')
+	end
+end
+
+function reverse_date()
+	vim.cmd('normal! dt/wwpldeBP')
+end
+
+map("n", "<leader>h1", ":lua doc_quick_underline(1)<cr>", { silent = true });
+map("n", "<leader>h2", ":lua doc_quick_underline(2)<cr>", { silent = true });
+
+
+map("n", "<leader>ct", ":ChecklistToggleCheckbox<cr>", { silent = true });
+map("n", "<leader>ce", ":ChecklistEnableCheckbox<cr>", { silent = true });
+map("n", "<leader>cd", ":ChecklistDisableCheckbox<cr>", { silent = true });
+map("n", "<leader>ct", ":ChecklistToggleCheckbox<cr>", { silent = true });
+map("n", "<leader>ce", ":ChecklistEnableCheckbox<cr>", { silent = true });
+map("n", "<leader>cd", ":ChecklistDisableCheckbox<cr>", { silent = true });
+
+-- vim.g.checklist_filetypes:append('adoc', 'md')
+
+-- vim-fugitive
+map("n", "<leader>gg", ":Ggrep<space>", { silent = false });
+map("n", "<leader>lg", ":Glgrep<space>", { silent = false });
